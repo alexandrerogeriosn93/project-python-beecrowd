3c65a8a46d421dfc6a5d0934b4c13301b6f7a81b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-18 19:13:37 -0300

    feat: problem 1064 Positivos e média

---
+++ beginer/list_2/1064/app.py
@@ -0,0 +1,12 @@
+quantity_positives = 0
+sum_positives = 0
+
+for i in range(6):
+    number = float(input())
+    if number > 0:
+        sum_positives += number 
+        quantity_positives += 1
+
+average_positives = sum_positives / quantity_positives
+
+print(f"{quantity_positives} valores positivos\n{average_positives:.1f}")
