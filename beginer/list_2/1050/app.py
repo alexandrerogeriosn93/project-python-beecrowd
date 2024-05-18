010a7cb9485c8b639fbab29d647cb05b2c1e82cc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-18 17:01:57 -0300

    feat: problem 1050 DDD

---
+++ beginer/list_2/1050/app.py
@@ -0,0 +1,13 @@
+code = int(input())
+ddds = {
+    11: "Sao Paulo",
+    19: "Campinas",
+    21: "Rio de Janeiro",
+    27: "Vitoria",
+    31: "Belo Horizonte",
+    32: "Juiz de Fora",
+    61: "Brasilia",
+    71: "Salvador",
+}
+
+print(ddds[code]) if code in ddds else print("DDD nao cadastrado")
