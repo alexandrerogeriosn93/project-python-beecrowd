7705c5810b4be04031d95380f19d384d77d6bb58
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-18 15:31:24 -0300

    feat: problem 1048 Aumento de salário

---
+++ beginer/list_2/1048/app.py
@@ -0,0 +1,18 @@
+salary = float(input())
+percent = 0
+
+if salary <= 400.00:
+    percent = 15
+elif salary <= 800.00:
+    percent = 12
+elif salary <= 1200.00:
+    percent = 10
+elif salary <= 2000.00:
+    percent = 7
+else:
+    percent = 4
+
+salary_adjustment = salary * (percent/100)
+new_salary = salary + salary_adjustment
+
+print(f"Novo salario: {new_salary:.2f}\nReajuste ganho: {salary_adjustment:.2f}\nEm percentual: {percent} %")
