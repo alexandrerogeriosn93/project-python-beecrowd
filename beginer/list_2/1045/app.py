5d1acb69c0003ba24fe02361608d590443904f4b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-18 13:19:37 -0300

    feat: problem 1045 Tipos de triângulos

---
+++ beginer/list_2/1045/app.py
@@ -0,0 +1,23 @@
+from math import pow
+
+values = list(map(float, input().split()))
+a, b, c = sorted(values, reverse=True)
+d = pow(a, 2)
+e = pow(b, 2) + pow(c, 2)
+
+if a >= b + c:
+    print("NAO FORMA TRIANGULO")
+else:
+    if d == e:
+        print("TRIANGULO RETANGULO")
+    
+    if d > e:
+        print("TRIANGULO OBTUSANGULO")
+    
+    if d < e:
+        print("TRIANGULO ACUTANGULO")
+    
+    if a == b and a == c:
+        print("TRIANGULO EQUILATERO")
+    elif a == b or a == c or b == c:
+        print("TRIANGULO ISOSCELES")
