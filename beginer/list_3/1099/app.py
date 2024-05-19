5601049b1499c84f3fb4f21a452ed184e0ebd544
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-19 14:11:26 -0300

    fix: refact problem 1099

--- beginer/list_3/1099/app.py
+++ beginer/list_3/1099/app.py
@@ -4,14 +4,11 @@ for i in range(n):
     x, y = map(int, input().split())
     sum_odds = 0
 
-    if x == y:
-        print(sum_odds)
-    else:
-        if x > y:
-            x, y = y, x
+    if x > y:
+        x, y = y, x
 
-        for j in range(x + 1, y):
-            if j % 2 != 0:
-                sum_odds += j
+    for j in range(x + 1, y):
+        if j % 2 != 0:
+            sum_odds += j
 
-        print(sum_odds)
+    print(sum_odds)
