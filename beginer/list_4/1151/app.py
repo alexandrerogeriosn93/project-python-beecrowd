a53b648d8bf75e7f8d4b4031f2e8115c1781a5b1
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-20 05:53:55 -0300

    feat: problem 1151 Fibonacci fácil

---
+++ beginer/list_4/1151/app.py
@@ -0,0 +1,18 @@
+def calc_fibonacci(n):
+    a, b = 0, 1
+
+    if n == 0:
+        return 0
+    elif n== 1:
+        return b
+    else:
+        for i in range(1, n):
+            c = a + b
+            a, b = b, c
+        return b
+
+n = int(input())
+
+for i in range(0, n):
+    fib = calc_fibonacci(i)
+    print(fib, end=[" ", "\n"][i == n - 1])
