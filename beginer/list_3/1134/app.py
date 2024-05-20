bd776f9e43e10fb017568f59ee80cde794ef15b5
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-20 04:20:44 -0300

    feat: problem 1134 Tipo de combustível

---
+++ beginer/list_3/1134/app.py
@@ -0,0 +1,12 @@
+alcohol, gasoline, diesel = 0, 0, 0
+
+while (code := int(input())) != 4:
+    match code:
+        case 1:
+            alcohol += 1
+        case 2:
+            gasoline += 1
+        case 3:
+            diesel += 1
+
+print(f"MUITO OBRIGADO\nAlcool: {alcohol}\nGasolina: {gasoline}\nDiesel: {diesel}")
