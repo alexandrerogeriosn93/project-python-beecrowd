42bd3b1b2feb071d6b7b2e27a1e2908b6bfe07f6
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-20 05:26:04 -0300

    feat: problem 1150 Ultrapassando Z

---
+++ beginer/list_4/1150/app.py
@@ -0,0 +1,17 @@
+x = int(input())
+z = x - 1
+i = 2
+response = x
+s = 1
+
+while z <= x:
+    z = int(input())
+
+while response <= z:
+    response += x + s
+
+    if response <= z:
+        i += 1
+        s += 1 
+
+print(i)
