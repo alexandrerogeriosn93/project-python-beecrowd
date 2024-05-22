2be5ac7e266053e8d5a5d1a80d0cf0fd1de90e4d
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-22 09:40:27 -0300

    feat: problem 1534 Matriz 123

---
+++ beginer/list_5/1534/app.py
@@ -0,0 +1,24 @@
+while True:
+    try:
+        n = int(input())
+        mat = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    mat[i][j] = 1
+
+                if i == n - j - 1:
+                    mat[i][j] = 2
+
+                if i != j and i != n - j -1:
+                    mat[i][j] = 3
+
+        for i in range(n):
+            text = ""
+            for j in range(n):
+                text += str(mat[i][j])
+            print(text)
+
+    except EOFError:
+        break
