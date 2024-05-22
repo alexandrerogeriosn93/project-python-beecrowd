ca4a1362851eca687188f17cda49f995cd01eb64
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-22 09:25:32 -0300

    feat: problem 1478 Matriz quadrada 2

---
+++ beginer/list_5/1478/app.py
@@ -0,0 +1,17 @@
+while (n := int(input())) > 0:
+    mat = []
+
+    for i in range(1, (n + 1)):
+        mat_aux = []
+        counter = i
+        for j in range(n):
+            mat_aux.append(abs(counter))
+            counter = counter - 3 if counter == 1 else counter - 1
+        mat.append(mat_aux)
+            
+    for i in range(n):
+        text = ""
+        for j in range(n):
+            text += " %3d" % mat[i][j]
+        print(text[1:])
+    print("")
