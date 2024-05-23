bfd9ee33513ca0a80dab67fa34c5501bcc31257e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-22 22:17:49 -0300

    feat: problem 1847 Bem-vindos e bem-vindas ao Inverno

---
+++ beginer/list_5/1847/app.py
@@ -0,0 +1,17 @@
+a, b, c = map(int, input().split())
+res = ""
+
+if a > b:
+    if c > b:
+        res = ':)'
+    else:
+        res = ':)' if b - c < a - b else ':('
+elif a < b:
+    if c < b:
+        res = ':('
+    else:
+        res = ':(' if b - c > a - b else ':)'
+else:
+    res = ':)' if c > a else ':('
+
+print(res)
