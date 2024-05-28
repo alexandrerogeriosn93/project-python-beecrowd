5c7c1fea6327fde51f7f868affd312090045040e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-27 23:19:16 -0300

    fix: problems 1007, 1151 and 2031

--- beginer/list_4/1151/app.py
+++ beginer/list_4/1151/app.py
@@ -3,7 +3,7 @@ def calc_fibonacci(n):
 
     if n == 0:
         return 0
-    elif n== 1:
+    elif n == 1:
         return b
     else:
         for i in range(1, n):
--- beginer/list_6/2031/app.py
+++ beginer/list_6/2031/app.py
@@ -4,20 +4,20 @@ for i in range(n):
     player_1 = input().strip().lower()
     player_2 = input().strip().lower()
 
-    if(player_1 == "ataque"):
-        if(player_2 == "ataque"):
+    if player_1 == "ataque":
+        if player_2 == "ataque":
             print("Aniquilacao mutua")
         else:
             print("Jogador 1 venceu")
-    elif(player_1 == "papel"):
-        if((player_2 == "ataque") or (player_2 == "pedra")):
+    elif player_1 == "papel":
+        if player_2 == "ataque" or player_2 == "pedra":
             print("Jogador 2 venceu")
         else:
             print("Ambos venceram")
-    elif(player_1 == "pedra"):
-        if(player_2 == "papel"):
+    elif player_1 == "pedra":
+        if player_2 == "papel":
             print("Jogador 1 venceu")
-        if(player_2 == "ataque"):
+        if player_2 == "ataque":
             print("Jogador 2 venceu")
-        elif(player_2 == "pedra"):
+        elif player_2 == "pedra":
             print("Sem ganhador")
