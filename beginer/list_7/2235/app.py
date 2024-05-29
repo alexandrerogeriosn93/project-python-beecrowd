88483cd4ddb0a8c1148614d985781487b5ef4a08
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-28 22:40:36 -0300

    feat: problem 2235 Andando no tempo

---
+++ beginer/list_7/2235/app.py
@@ -0,0 +1,6 @@
+def check_condition(a, b, c):
+    return a + b == c or a + c == b or b + c == a or a == b or a == c or b == c
+
+a, b, c = map(int, input().split())
+
+print("S") if check_condition(a, b, c) else print("N")
