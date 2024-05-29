bf9d257aae65566b7807d85caf423306bd4eca4d
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-05-28 21:34:15 -0300

    feat: problem 2203 Tempestade de corvos

---
+++ beginer/list_7/2203/app.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+while True:
+    try:
+        x1, y1, x2, y2, vi, r1, r2 = map(int, input().split())
+        distance = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        distance += vi * 1.5
+        radius = float(r1 + r2)
+
+        print("N") if distance > radius else print("Y")
+    except EOFError:
+        break
