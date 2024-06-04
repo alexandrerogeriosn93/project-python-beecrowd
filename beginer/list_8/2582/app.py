c8d73559090cfb07a58edde25813575b5f40caea
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-04 18:09:33 -0300

    feat: problem 2582 System of a Download

---
+++ beginer/list_8/2582/app.py
@@ -0,0 +1,19 @@
+musics = {
+    0: "PROXYCITY",
+    1: "P.Y.N.G.",
+    2: "DNSUEY!",
+    3: "SERVERS",
+    4: "HOST!",
+    5: "CRIPTONIZE",
+    6: "OFFLINE DAY",
+    7: "SALT",
+    8: "ANSWER!",
+    9: "RAR?",
+    10: "WIFI ANTENNAS"
+}
+
+c = int(input())
+
+for _ in range(c):
+    x, y = map(int, input().split())
+    print(musics.get(x + y))
