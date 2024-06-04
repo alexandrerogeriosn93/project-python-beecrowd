7af9bd11636a4a495591d08d81890d31a4248f15
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-04 14:56:32 -0300

    feat: problem 2542 Iu-Di-Oh

---
+++ beginer/list_8/2542/app.py
@@ -0,0 +1,22 @@
+# https://github.com/xTecna/solucoes-da-beecrowd/blob/main/problemas/iniciante/2542/README.md
+
+while True:
+    try:
+        n = int(input())
+        marcos, leonardo = map(int, input().split())
+        cards_marcos = [list(map(int, input().split())) for _ in range(marcos)]
+        cards_leonardo = [list(map(int, input().split())) for _ in range(leonardo)]
+        cm, cl = map(int, input().split())
+        a = int(input())
+
+        marcos_card = cards_marcos[cm - 1][a - 1]
+        leonardo_card = cards_leonardo[cl - 1][a - 1]
+
+        if marcos_card > leonardo_card:
+            print("Marcos")
+        elif leonardo_card > marcos_card:
+            print("Leonardo")
+        else:
+            print("Empate")
+    except EOFError:
+        break
