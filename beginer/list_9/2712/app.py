5185cf9f62e6c91e4caabd45edd899eb28fbbab2
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-09 17:38:26 -0300

    feat: problem 2712 Rodízio veicular

---
+++ beginer/list_9/2712/app.py
@@ -0,0 +1,19 @@
+import re
+
+days = {
+    i: ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"][(i-1)//2]
+    for i in range(10)
+}
+
+def validate_plate(plate):
+    return re.match("^[A-Z]{3}-[0-9]{4}$", plate) is not None
+
+def get_day(plate):
+    return days[int(plate[7])]
+
+n = int(input())
+
+for _ in range(n):
+    s = input()
+
+    print(f"FAILURE" if not validate_plate(s) else get_day(s))
