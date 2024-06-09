4deb799fd64563653832a9f2197e26618a28272a
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-09 20:41:02 -0300

    feat: problem 2721 Indecisão das renas

---
+++ beginer/list_9/2721/app.py
@@ -0,0 +1,16 @@
+reindeer = [
+    "Rudolph",
+    "Dasher",
+    "Dancer",
+    "Prancer",
+    "Vixen",
+    "Comet",
+    "Cupid",
+    "Donner",
+    "Blitzen"
+]
+
+total_sum = sum(list(map(int, input().split())))
+answer = reindeer[total_sum % 9]
+
+print(answer)
