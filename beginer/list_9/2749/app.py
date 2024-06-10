4ca41c1a108de52aa6b35d20d6429c0a2a4e2a0e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-10 17:58:21 -0300

    feat: problems 2749 and 2750 Saída 3 e Saída 4

---
+++ beginer/list_9/2749/app.py
@@ -0,0 +1,17 @@
+def print_line(width=39):
+    print("-" * width)
+
+def print_center(text, index):
+    index = max(2, min(37, index))
+    spaces_left = index - 2
+    spaces_right = 39 - (spaces_left + len(text)) - 2
+
+    print(f'|{" " * spaces_left}{text}{" " * spaces_right}|')
+
+print_line()
+print_center("x = 35", 1)
+print_center("", 1)
+print_center("x = 35", 17)
+print_center("", 1)
+print_center("x = 35", 33)
+print_line()
