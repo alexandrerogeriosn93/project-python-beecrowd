95134587ca70e71d2dbcc95a5783e40d9193adad
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-17 09:44:01 -0300

    feat: problem 2774 Precisão do sensor

---
+++ beginer/list_10/2774/app.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        hm = map(int, input().split())
+        values = list(map(float, input().split()))
+        
+        average = sum(values)/len(values)
+        answer = 0
+
+        for value in values:
+            answer += (value - average) ** 2
+
+        answer = (answer / (len(values) - 1)) ** (1 / 2)
+
+        print(f"{answer:.5f}")
+    except EOFError:
+        break
