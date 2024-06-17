35a1c024aa736afba6a4e3070c0cd3d3e9eaccba
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-17 09:32:51 -0300

    feat: problem 2770 Tamanho da placa

---
+++ beginer/list_10/2770/app.py
@@ -0,0 +1,28 @@
+import sys
+
+def validate(x, xi, y, yi):
+    return xi <= x and yi <= y or xi <= y and yi <= x
+
+input = sys.stdin.read().strip().split()
+
+index = 0
+results = []
+
+while index < len(input):
+    try:
+        x = int(input[index])
+        y = int(input[index + 1])
+        m = int(input[index + 2])
+        index += 3
+        
+        for _ in range(m):
+            xi = int(input[index])
+            yi = int(input[index + 1])
+            index += 2
+        
+            results.append("Sim") if validate(x, xi, y, yi) else results.append("Nao")
+    
+    except ValueError:
+        break
+
+sys.stdout.write("\n".join(results) + "\n")
