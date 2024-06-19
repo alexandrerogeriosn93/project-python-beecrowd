9357a1acffd6de6b95192fb28552a9e90613afcc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-19 07:54:52 -0300

    feat: problem 2785 Pirâmide

---
+++ beginer/list_10/2785/app.py
@@ -0,0 +1,21 @@
+n = int(input())
+mat = [[int(x) for x in input().strip().split()] for _ in range(n)]
+weights = [mat[0]]
+
+for c in range(1, n):
+    i = 0
+    j = i + c + 1
+    weights.append([])
+
+    for d in range(n - c):
+        first_element = weights[c - 1][d]
+        second_element = weights[c - 1][d + 1]
+        res_element = first_element if first_element < second_element else second_element
+        e = sum(mat[c][i:j]) + res_element
+        
+        weights[c].append(e)
+        i += 1
+        j += 1
+
+min_total_weight = weights[-1][-1]
+print(min_total_weight)
