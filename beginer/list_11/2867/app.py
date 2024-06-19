c430f81fb2515498358804e56a490cbc909cdc36
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-19 11:33:49 -0300

    feat: problem 2867 Dígitos

---
+++ beginer/list_11/2867/app.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        c = int(input())
+
+        for _ in range(c):
+            n, m = map(int, input().split())
+            print(len(str(int(n ** m))))
+    except EOFError:
+        break
