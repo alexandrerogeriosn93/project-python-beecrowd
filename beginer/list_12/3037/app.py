abf1db3278e11528379764e4dd4c5dd12959b62f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-21 10:33:17 -0300

    feat: problem 3037 Jogando dardos por distância

---
+++ beginer/list_12/3037/app.py
@@ -0,0 +1,21 @@
+def calc_values(i):
+    player = 0
+
+    for _ in range(i):
+        x, d = map(int, input().split())
+        player += x * d
+
+    return player
+
+n = int(input())
+
+for _ in range(n):
+    j = m = 0
+
+    j = calc_values(3)
+    m = calc_values(3)
+
+    if j > m:
+        print("JOAO")
+    elif m > j:
+        print("MARIA")
