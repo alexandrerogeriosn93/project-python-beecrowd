0030aeb8ab368a826ac862689237942f9da61d6e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-06-23 08:09:05 -0300

    feat: problem 3249 Death knight hero

---
+++ beginer/list_13/3249/app.py
@@ -0,0 +1,17 @@
+def count_wins(battles):
+    wins = 0
+    for battle in battles:
+        if "CD" not in battle:
+            wins += 1
+    return wins
+
+n = int(input())
+battles = []
+
+for _ in range(n):
+    battle_skills = input().strip()
+    battles.append(battle_skills)
+
+num_wins = count_wins(battles)
+
+print(num_wins)
