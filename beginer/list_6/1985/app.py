f79ac77ae781d9f79ccd3f110d15207999d89955
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-10 00:39:33 -0300

    fix: adjust problem 1985

--- beginer/list_6/1985/app.py
+++ beginer/list_6/1985/app.py
@@ -6,14 +6,14 @@ for i in range(n):
 
     match code:
         case 1001:
-            total += (quantity * 1.50)
+            total += quantity * 1.50
         case 1002:
-            total += (quantity * 2.50)
+            total += quantity * 2.50
         case 1003:
-            total += (quantity * 3.50)
+            total += quantity * 3.50
         case 1004:
-            total += (quantity * 4.50)
+            total += quantity * 4.50
         case 1005:
-            total += (quantity * 5.50)
+            total += quantity * 5.50
 
 print(f"{total:.2f}")
