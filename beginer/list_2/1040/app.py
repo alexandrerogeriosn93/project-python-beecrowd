40daeb59a5cf7c93ed0f3f043732d2e990999f99
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-09 21:57:55 -0300

    fix: adjust problem 1040

--- beginer/list_2/1040/app.py
+++ beginer/list_2/1040/app.py
@@ -1,6 +1,6 @@
 N1, N2, N3, N4 = map(float, input().split())
 
-average = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10
+average = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10
 print(f"Media: {average:.1f}")
 
 if average < 5.0:
@@ -14,6 +14,5 @@ else:
     print(f"Nota do exame: {N5:.1f}")
     average_final = (N5 + average) / 2
 
-    res = "Aluno aprovado." if average_final >= 5.0 else "Aluno reprovado."
-    print(res)
+    print("Aluno aprovado." if average_final >= 5.0 else "Aluno reprovado.")
     print(f"Media final: {average_final:.1f}")
