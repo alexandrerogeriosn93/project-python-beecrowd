62c4d3383b5c31e199e4219f2a1ec446ece2bb1c
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-10 00:47:22 -0300

    fix: refact problem 2059

--- beginer/list_6/2059/app.py
+++ beginer/list_6/2059/app.py
@@ -1,21 +1,12 @@
 p, j1, j2, r, a = map(int, input().split())
 
 if r == 1:
-    if a == 1:
-        print("Jogador 2 ganha!")
-    else:
-        print("Jogador 1 ganha!")
+    print("Jogador 2 ganha!" if a == 1 else "Jogador 1 ganha!")
 else:
     if a == 1:
         print("Jogador 1 ganha!")
     else:
         if (j1 + j2) % 2 == 0:
-            if p == 0:
-                print("Jogador 2 ganha!")
-            else:
-                print("Jogador 1 ganha!")
+            print("Jogador 2 ganha!" if p == 0 else "Jogador 1 ganha!")
         else:
-            if p == 0:
-                print("Jogador 1 ganha!")
-            else:
-                print("Jogador 2 ganha!")
+            print("Jogador 1 ganha!" if p == 0 else "Jogador 2 ganha!")
