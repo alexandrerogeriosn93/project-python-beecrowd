72bc75eeae16a1047326edd50331bb0d5b2989a4
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-09 21:02:03 -0300

    fix: adjust problem 1019

--- beginer/list_1/1019/app.py
+++ beginer/list_1/1019/app.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 hours = n // 3600
-minutes = (n - (hours * 3600)) // 60
-seconds = n -((hours * 3600) + (minutes * 60))
+minutes = (n - hours * 3600) // 60
+seconds = n - (hours * 3600 + minutes * 60)
 
 print(f"{hours}:{minutes}:{seconds}")
