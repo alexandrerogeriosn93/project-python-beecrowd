706f5c9fb1b20cd92441655aa2f4f0e9e61c3b8d
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-09 22:42:22 -0300

    fix: adjust problem 1043

--- beginer/list_2/1043/app.py
+++ beginer/list_2/1043/app.py
@@ -1,5 +1,5 @@
 def is_triangle(a, b, c):
-    return (a < b + c) and (b < a + c) and (c < a + b)
+    return a < b + c and b < a + c and c < a + b
 
 def calc_area(a, b, c):
     return ((a + b) * c) / 2 
@@ -9,6 +9,4 @@ def calc_perimeter(a, b, c):
 
 a, b, c = map(float, input().split())
 
-res =  f"Perimetro = {calc_perimeter(a, b, c):.1f}" if is_triangle(a, b, c) else f"Area = {calc_area(a, b, c):.1f}"
-
-print(res)
+print(f"Perimetro = {calc_perimeter(a, b, c):.1f}" if is_triangle(a, b, c) else f"Area = {calc_area(a, b, c):.1f}")
