94360fb46566d79f1d0eaf101bbfac9785d14a61
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-10 00:53:40 -0300

    fix: adjust problem 2143

--- beginer/list_7/2143/app.py
+++ beginer/list_7/2143/app.py
@@ -2,4 +2,4 @@ while (t := int(input())) != 0:
     for i in range(1, t + 1):
         n = int(input())
 
-        print(f"{(n * 2) - 1}") if n % 2 != 0 else print(f"{(n * 2) - 2}")
+        print(f"{(n * 2) - 1}" if n % 2 != 0 else f"{(n * 2) - 2}")
