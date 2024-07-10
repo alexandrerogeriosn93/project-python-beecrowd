44fc025b618bac679ac421fb5f5b539e7c1f4435
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-10 02:00:21 -0300

    fix: adjust problem 2774

--- beginer/list_10/2774/app.py
+++ beginer/list_10/2774/app.py
@@ -2,7 +2,6 @@ while True:
     try:
         hm = map(int, input().split())
         values = list(map(float, input().split()))
-        
         average = sum(values)/len(values)
         answer = 0
 
@@ -10,7 +9,6 @@ while True:
             answer += (value - average) ** 2
 
         answer = (answer / (len(values) - 1)) ** (1 / 2)
-
         print(f"{answer:.5f}")
     except EOFError:
         break
