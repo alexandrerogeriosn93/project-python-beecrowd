a7979a9e7c1bbd2596402b52736cc93173512f3b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-07-09 21:03:39 -0300

    fix: adjust problem 1020

--- beginer/list_1/1020/app.py
+++ beginer/list_1/1020/app.py
@@ -1,7 +1,7 @@
 age = int(input())
 
 year = age // 365
-month = (age - (year * 365)) // 30
-day = age - (year * 365) - (month * 30)
+month = (age - year * 365) // 30
+day = age - year * 365 - month * 30
 
 print(f"{year} ano(s)\n{month} mes(es)\n{day} dia(s)")
